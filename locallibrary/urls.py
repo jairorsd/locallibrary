531fc90d4dbb05b5393a228cd61823a16d184cfb
Jairo <paypal061994@gmail.com>
2023-12-05 04:57:24 +0000

    criado view index e template html

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -23,6 +23,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path('catalog/', include('catalog.urls')),
-    	path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls'))
+]
